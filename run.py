ffd885e16bd972c69ef79250f706e292bbd12923
Gordon Lemmon <glemmon@motivemi.com>
2025-08-25 14:33:17 -0500

    Expose app creation

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import create_app
 import os
 
+app = create_app()
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
-    host = os.environ.get("HOST", "0.0.0.0")
-    app = create_app()
-    app.run(host=host, port=port, debug=os.environ.get("DEBUG", "0") == "1")
+    app.run(debug=True)
